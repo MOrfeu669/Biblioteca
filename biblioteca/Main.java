31abec53f77371995850e1ffb2b4fdd4174cb156
Bernardo Gatto <119093135+MOrfeu669@users.noreply.github.com>
2024-05-16 15:20:25 -0300

    Update Main.java
    commit

--- biblioteca/Main.java
+++ biblioteca/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("3. Exit");
 
             int choice = scan.nextInt();
-            scan.nextLine();
+            scan.nextLine(); // Consume newline
 
             switch (choice) {
                 case 1:
